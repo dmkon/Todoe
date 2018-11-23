8df155e49a7199b8e2fa64fa0eb8be722c4ddffe
dmkon <dmkon@hotmail.com>
2018-11-22 22:33:04 -0500

    Persisting data using Realm

---
+++ Todoe/Data Model/Category.swift
@@ -0,0 +1,15 @@
+//
+//  Category.swift
+//  Todoe
+//
+//  Created by admin on 2018-11-20.
+//  Copyright © 2018 nortohol. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Category: Object {
+    @objc dynamic var name: String = ""
+    let items = List<Item>()
+}
